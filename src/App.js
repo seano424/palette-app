53b61d3f66b4352e61aabcccb953650284556f68
Sean OReilly <soreilly424@gmail.com>
2021-04-10 16:51:41 -0500

    added slider from rc-slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import seeds from './seedPalette'
 import Palette from './Palette'
+import { generatePalette } from "./colorHelpers";
+
 
 function App() {
+  console.log(generatePalette(seeds[2]))
   return (
     <div>
-      <Palette {...seeds[2]}/>
+      <Palette palette={generatePalette(seeds[2])}/>
     </div>
   );
 }
--- src/Palette.js
+++ src/Palette.js
@@ -1,19 +1,39 @@
 import React, { Component } from "react";
-import ColorBox from './ColorBox'
-import './Palette.css'
+import ColorBox from "./ColorBox";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+import "./Palette.css";
 
 export default class Palette extends Component {
-  render() {
+  state = {
+    level: 500,
+  };
 
-    const colors = this.props.colors.map(color => (
-      <ColorBox background={color.color} name={color.name}/>
-    ))
-    
+  handleSlider = (level) => {
+    console.log(level);
+    this.setState({ level });
+  };
+
+  render() {
+    const { colors } = this.props.palette;
+    const { level } = this.state;
+
+    const colorBox = colors[level].map((color) => (
+      <ColorBox key={color.name} background={color.hex} name={color.name} />
+    ));
+
     return (
       <div className="Palette">
-        <div class="Palette-colors">
-          {colors}
+        <div class="slider">
+          <Slider
+            defaultValue={500}
+            min={100}
+            max={900}
+            step={100}
+            onAfterChange={this.handleSlider}
+          />
         </div>
+        <div className="Palette-colors">{colorBox}</div>
       </div>
     );
   }
