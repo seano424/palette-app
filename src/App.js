4f7d2e3dea5e7522ca45f4be70017bad5292dcf0
Sean OReilly <soreilly424@gmail.com>
2021-04-11 13:16:49 -0500

    fixed palette route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import seeds from './seedPalette'
-import Palette from './Palette'
+import seeds from "./seedPalette";
+import Palette from "./Palette";
 import { generatePalette } from "./colorHelpers";
-import { Route } from 'react-router-dom'
-
+import { Route, Switch } from "react-router-dom";
 
 function App() {
-  console.log(generatePalette(seeds[2]))
+  console.log(seeds);
+  const findPalette = (id) => {
+    return seeds.find(seed => seed.id === id)
+  }
   return (
     <div>
-      <Route exact path="/" render={() => <h1>All PALETTES</h1>} />
-      <Route exact path="/palette/:id" render={() => <h1>INDIVIDUAL PALETTE</h1>} />
-      {/* <Palette palette={generatePalette(seeds[2])}/> */}
+      <Switch>
+        <Route exact path="/" render={() => <h1>All PALETTES</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={(routeParams) => <Palette palette={generatePalette(findPalette(routeParams.match.params.id))}/>}
+        />
+      </Switch>
     </div>
   );
 }
